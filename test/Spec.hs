1bcce29624f72033ba31bd82e17857435d98ad98
minato225 <1815131bill@gmail.com>
2022-10-05 13:17:34 +0300

    more succinctly test

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,22 +2,13 @@ import Test.HUnit
 import System.Exit
 import Factorial as F
 
-testZeroCase :: Test
-testZeroCase = 
-    TestCase $ assertEqual "Factorial 1" 
-        1 $ F.factorial 1
-        
-testNonZeroCase :: Test
-testNonZeroCase = 
-    TestCase $ assertEqual "Factorial 10"
-         3628800 $ F.factorial 10
+tests = test [  
+    "test1" ~: "(Factorial 1)" ~: 1 ~=? F.factorial 1,
+    "test2" ~: "(Factorial 10)" ~: 3628800 ~=? F.factorial 10 ]
 
 main :: IO ()
 main = do
-        counts <- runTestTT ( test [
-            testZeroCase,
-            testNonZeroCase
-            ])
+        counts <- runTestTT tests
         if errors counts + failures counts == 0
             then exitSuccess
             else exitFailure
